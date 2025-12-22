f7bb1b2cabba028ba4ec55dbdb3921e658e65f35
singh-ip <inderpreetnarula@yahoo.com>
2025-12-22 11:06:44 +0530

    Creating Money class basis the failing test cases

---
+++ src/ValueObjects/Money.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KikiCourier\ValueObjects;
+
+final class Money
+{
+    private float $amount;
+
+    public function __construct(float $amount)
+    {
+        if ($amount < 0) {
+            throw new \InvalidArgumentException('Amount cannot be negative');
+        }
+        
+        $this->amount = round($amount, 2);
+    }
+
+    public function getAmount(): float
+    {
+        return $this->amount;
+    }
+
+    public function add(Money $money): Money
+    {
+        return new Money($this->amount + $money->getAmount());
+    }
+
+    public function subtract(Money $money): Money
+    {
+        return new Money($this->amount - $money->getAmount());
+    }
+
+    public function percentage(float $percent): Money
+    {
+        return new Money($this->amount * ($percent / 100));
+    }
+}
